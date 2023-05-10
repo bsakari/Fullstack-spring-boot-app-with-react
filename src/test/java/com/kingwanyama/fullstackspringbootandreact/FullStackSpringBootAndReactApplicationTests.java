acb491a8f946092c1f087f749e344ccef0524613
bsakari <sakaribenjamin@gmail.com>
2023-05-10 07:19:28 +0300

    make test fail

--- src/test/java/com/kingwanyama/fullstackspringbootandreact/FullStackSpringBootAndReactApplicationTests.java
+++ src/test/java/com/kingwanyama/fullstackspringbootandreact/FullStackSpringBootAndReactApplicationTests.java
@@ -1,5 +1,6 @@
 package com.kingwanyama.fullstackspringbootandreact;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class FullStackSpringBootAndReactApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("Ooops!!! Tests failed!!!");
 	}
 
 }
