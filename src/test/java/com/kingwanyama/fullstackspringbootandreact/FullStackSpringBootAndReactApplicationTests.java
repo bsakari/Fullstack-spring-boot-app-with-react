a0b76ca3ad7f1058b154ec787d30d375514c3514
Benjamin Sakari <sakaribenjamin@gmail.com>
2023-05-10 07:31:05 +0300

    Revert "make test fail"

--- src/test/java/com/kingwanyama/fullstackspringbootandreact/FullStackSpringBootAndReactApplicationTests.java
+++ src/test/java/com/kingwanyama/fullstackspringbootandreact/FullStackSpringBootAndReactApplicationTests.java
@@ -1,6 +1,5 @@
 package com.kingwanyama.fullstackspringbootandreact;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +8,6 @@ class FullStackSpringBootAndReactApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Assertions.fail("Ooops!!! Tests failed!!!");
 	}
 
 }
