081cbcc0daea0f603f77987f12bf3422c2a94fe5
bsakari <sakaribenjamin@gmail.com>
2023-05-15 06:51:53 +0300

    Edited pom file

--- src/test/java/com/kingwanyama/fullstackspringbootandreact/FullStackSpringBootAndReactApplicationTests.java
+++ src/test/java/com/kingwanyama/fullstackspringbootandreact/FullStackSpringBootAndReactApplicationTests.java
@@ -3,12 +3,16 @@ package com.kingwanyama.fullstackspringbootandreact;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class FullStackSpringBootAndReactApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 		Assertions.fail("Ooops test failed");
 	}
 
